f259675c054da25c5929d356e3cded5eb5d49e89
Martin <martyofmca@gmail.com>
2024-02-02 21:28:49 +0000

    fix: validate least significant bits of data - 0x04

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -20,8 +20,29 @@ def validUTF8(data):
         True when `data` contains valid UTF-8
         character sets otherwise False.
     """
-    for item in data:
-        if (item > 127):
-            return (False)
+    # Start by assuming the codepoint contains
+    # only 1 chunk
+    chunk = 1
 
-    return (True)
+    for item in data:
+        if (chunk == 1):
+            # Determine the number of chunks
+            if (item >> 5 == 0b110 or item >> 5 == 0b1110):
+                chunk = 2
+            elif (item >> 4 == 0b1110):
+                chunk = 3
+            elif (item >> 3 == 0b11110):
+                chunk = 4
+            elif (item >> 7 & 0b1):
+                return (False)
+        else:
+            # Check if continuation bytes of
+            # the multi-byte chunk is valid
+            if (item >> 6 != 0b10):
+                return (False)
+
+            # Ensure the next continuation
+            # byte is checked
+            chunk -= 1
+
+    return (chunk == 1)
