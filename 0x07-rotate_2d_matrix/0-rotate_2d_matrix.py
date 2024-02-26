c1edf8ef58bb018a95744ea83123bdb075acc604
Martin <martyofmca@gmail.com>
2024-02-25 17:34:40 -0800

    rotate 2D matrix - 0x07

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Define a module that implements a funciton
+to rotate a matrix by 90 degrees.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate the given array by 90 degrees.
+
+    Parameters:
+        matrix : list
+        A list of lists containing
+        the original matrix.
+    """
+    for counter in range(len(matrix)):
+        matrix[counter].reverse()
+
+    for row in range(len(matrix)):
+        for col in range(row, len(matrix)):
+            matrix[row][col], matrix[col][row] = matrix[col][row],\
+                    matrix[row][col]
+
+    for counter in range(len(matrix)):
+        matrix[counter].reverse()
+
+    matrix.reverse()
